ebe24330c176bb53e1fdbeb516b5b16f98669c7d
alchemist2016 <vukdragovic@hotmail.com>
2020-03-05 15:40:11 +0000

    Fixed bug with datepicker and validated code

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,10 @@ function myFunction() {
     document.getElementById("verify").innerHTML = inpObj.validationMessage;
   }
 }
+
+function myFunction() {
+  var inpObj = document.getElementById("date_joined");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("verify").innerHTML = inpObj.validationMessage;
+  }
+}
